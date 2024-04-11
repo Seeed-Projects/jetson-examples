d4320d93a5ad1f418c6f9d6caff6dd45dd573162
luozhixin <zhixin.luo@seeed.cc>
2024-04-11 11:23:12 +0800

    feat: install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# TODO: make sure python3 in host is OK
+cd /tmp && \
+git clone https://github.com/luoluoter/jetson-examples && \
+cd jetson-examples && \
+pip install . && \
+rm -rf /tmp/jetson-examples && \
+echo "reComputer installed. try 'reComputer run whisper' to enjoy!"
