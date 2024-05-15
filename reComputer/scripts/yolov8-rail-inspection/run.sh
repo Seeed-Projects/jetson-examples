650054fca8d528f1db4641a5b0fd27dce83bd573
yaohui1998 <510677932@qq.com>
2024-05-15 03:28:21 +0000

    Add: Added YOLOv8 integration for rail inspection

--- reComputer/scripts/yolov8-rail-inspection/run.sh
+++ reComputer/scripts/yolov8-rail-inspection/run.sh
@@ -9,7 +9,7 @@ if [ "$(docker ps -aq -f name=yolov8_rain_inspection)" ]; then
     docker cp yolov8_rain_inspection:/usr/src/ultralytics/Jetson-example/result/ ~/
 else
     echo "No existing container named counter found. Pulling image and running container..."
-    docker run -it -d --network host \
+    docker run -it --rm --network host \
     --ipc=host \
     --runtime=nvidia \
     -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
@@ -19,6 +19,4 @@ else
     --name yolov8_rain_inspection \
     --device=/dev/*:/dev/*  \
     yaohui1998/bolt_inspection:1.0
-    docker exec yolov8_rain_inspection python3 bolt_inspection.py
-    docker cp yolov8_rain_inspection:/usr/src/ultralytics/Jetson-example/result/ ~/
 fi 
