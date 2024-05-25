863cb0d7c1785e7a02993dd972bcdf0b2f3f3622
youjiang <254693273@qq.com>
2024-05-25 16:50:36 +0800

    update: improve scalability

--- reComputer/scripts/text-generation-webui/init.sh
+++ reComputer/scripts/text-generation-webui/init.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+
 BASE_PATH=/home/$USER/reComputer
 mkdir -p $BASE_PATH/
 JETSON_REPO_PATH="$BASE_PATH/jetson-containers"
-BASE_JETSON_LAB_GIT="https://github.com/dusty-nv/jetson-containers/tree/d1573a3e8d7ba3fef36ebb23a7391e60eaf64db7"
+BASE_JETSON_LAB_GIT="https://github.com/dusty-nv/jetson-containers"
 if [ -d $JETSON_REPO_PATH ]; then
     echo "jetson-ai-lab existed."
 else
@@ -11,6 +12,5 @@ else
     cd $BASE_PATH/
     git clone --depth=1 $BASE_JETSON_LAB_GIT
     cd $JETSON_REPO_PATH
-    sudo apt update; sudo apt install -y python3-pip
-    pip3 install -r requirements.txt
+    bash install.sh
 fi
