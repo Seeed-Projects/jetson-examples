6e826b8d93f9d46d35e37b9057258bbcd0c5dc87
zibo <102125578+zibochen6@users.noreply.github.com>
2026-03-05 15:31:41 +0800

    update install_config

---
+++ setup.py
@@ -0,0 +1,46 @@
+from pathlib import Path
+
+from setuptools import setup
+
+
+README_PATH = Path(__file__).parent / "README.md"
+LONG_DESCRIPTION = README_PATH.read_text(encoding="utf-8")
+
+
+setup(
+    name="jetson-examples",
+    version="0.2.2",
+    author="luozhixin",
+    author_email="zhixin.luo@seeed.cc",
+    description="Running Gen AI models and applications on NVIDIA Jetson devices with one-line command",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.8",
+    keywords=[
+        "llama",
+        "llava",
+        "gpt",
+        "llm",
+        "nvidia",
+        "jetson",
+        "multimodal",
+        "jetson orin",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=["reComputer"],
+    include_package_data=True,
+    package_data={"reComputer": ["scripts/**/*"]},
+    entry_points={
+        "console_scripts": [
+            "reComputer=reComputer.main:run_script",
+        ]
+    },
+    project_urls={
+        "Homepage": "https://github.com/Seeed-Projects/jetson-examples",
+        "Issues": "https://github.com/Seeed-Projects/jetson-examples/issues",
+    },
+)
